65a055fe1541944dc95a91c0491538ba4b11437c
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2022-10-12 10:36:20 +0200

    Create integration test for regular link

---
+++ tests/src/Kernel/Concerns/OverridesRequestStack.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\actionlink_dropdown\Kernel\Concerns;
+
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Http\RequestStack;
+
+trait OverridesRequestStack {
+    protected function createRequestStack(): RequestStack {
+        /** @var RequestStack $requestStack */
+        $requestStack = \Drupal::service('request_stack');
+        while ($requestStack->getCurrentRequest()) {
+            $requestStack->pop();
+        }
+
+        $request = Request::create('/');
+        $requestStack->push($request);
+
+        return $requestStack;
+    }
+}
--- tests/src/Kernel/LocalActionRendererTest.php
+++ tests/src/Kernel/LocalActionRendererTest.php
@@ -5,30 +5,39 @@ declare(strict_types=1);
 namespace Drupal\Tests\actionlink_dropdown\Kernel;
 
 use Drupal\actionlink_dropdown\Render\LocalActionRenderer;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultAllowed;
 use Drupal\Core\Http\RequestStack;
 use Drupal\Core\Render\Markup;
 use Drupal\Core\Routing\RedirectDestination;
-use Drupal\Core\Routing\RouteMatch;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Routing\RouteProviderInterface;
 use Drupal\Core\Routing\UrlGeneratorInterface;
+use Drupal\Core\Url;
 use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
 use Drupal\menu_ui\Plugin\Menu\LocalAction\MenuLinkAdd;
+use Drupal\Tests\actionlink_dropdown\Kernel\Concerns\OverridesRequestStack;
 use Drupal\user\Entity\User;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Route;
 
 class LocalActionRendererTest extends EntityKernelTestBase {
+    use OverridesRequestStack;
+
     protected LocalActionRenderer $localActionRenderer;
     protected RouteProviderInterface $routeProvider;
     protected UrlGeneratorInterface $urlGenerator;
+    protected RequestStack $requestStack;
+    protected RouteMatchInterface $routeMatch;
 
     protected function setUp(): void {
         parent::setUp();
+
+        $this->requestStack = $this->createRequestStack();
 
         $this->enableModules(['actionlink_dropdown']);
         $this->setUpCurrentUser(['uid' => 1]);
 
         $this->localActionRenderer = \Drupal::service('actionlink_dropdown.renderer');
+        $this->routeMatch = \Drupal::service('current_route_match');
         $this->routeProvider = \Drupal::service('router.route_provider');
         $this->urlGenerator = \Drupal::service('url_generator');
     }
@@ -38,23 +47,42 @@ class LocalActionRendererTest extends EntityKernelTestBase {
             'id' => 'test_link',
             'title' => Markup::create('Test link'),
             'weight' => null,
-            'route_name' => '<front>',
+            'route_name' => 'user.admin_index',
             'route_parameters' => [],
             'options' => [],
-            'appears_on' => ['user.admin_index'],
+            'appears_on' => ['<front>'],
             'class' => 'Drupal\menu_ui\Plugin\Menu\LocalAction\MenuLinkAdd',
             'provider' => 'test_provider',
         ];
-        $request = Request::create('/admin/config/people');
-        $requestStack = new RequestStack();
-        $requestStack->push($request);
-        $redirectDestination = new RedirectDestination($requestStack, $this->urlGenerator);
 
+        $redirectDestination = new RedirectDestination($this->requestStack, $this->urlGenerator);
         $localAction = new MenuLinkAdd([], $pluginDefinition['id'], $pluginDefinition, $this->routeProvider, $redirectDestination);
-        $routeMatch = new RouteMatch('user.admin_index', new Route('/admin/config/people'));
+
         /** @var User $user */
         $user = User::load(1);
 
-        dump($this->localActionRenderer->createRenderElement($localAction, $routeMatch, $user, $pluginDefinition['title']->__toString()));
+        $renderElement = $this->localActionRenderer->createRenderElement(
+            $localAction,
+            $this->routeMatch,
+            $user,
+            $pluginDefinition['title']->__toString()
+        );
+
+        $expected = [
+            '#theme' => 'menu_local_action',
+            '#link' => [
+                'title' => 'Test link',
+                'url' => Url::fromRoute($localAction->getRouteName(), $localAction->getRouteParameters($this->routeMatch)),
+                'localized_options' => [
+                    'query' => [
+                        'destination' => '/',
+                    ],
+                ],
+            ],
+            '#access' => AccessResult::allowed()->addCacheContexts(['user.permissions']),
+            '#weight' => null,
+        ];
+
+        $this->assertEquals($expected, $renderElement);
     }
 }
